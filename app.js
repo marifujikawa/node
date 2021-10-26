8cb5a8bd03c2cf57d23fb7452ec9d23c9d85edf4
Mariana Fujikawa <mari.fujikawa97@gmail.com>
2021-10-26 15:25:52 -0300

    nodemon configured

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.send("Hello Watch!");
 });
 
 app.listen(3000, function () {
-    console.log("funciona na portinha 3000");
+    console.log("funciona na porta 3000");
 });
